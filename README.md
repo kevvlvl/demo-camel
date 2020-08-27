5cbe8e4b24bc6763fcd8c943ec0abb3ce8533069
Kevork <ksayad@gmail.com>
2020-08-27 12:59:42 -0400

    Added swagger

--- README.md
+++ README.md
@@ -3,14 +3,26 @@
 A simple spring-boot based project exposing rest endpoints using apache camel.
 The purpose was for me to experiment camel's routing/DSL capabilities with simple to understand examples.
 
-Endpoints:
-- GET api/health for custom API health check
-- POST api/clients to create a client
-- GET api/clients/{clientId} to get a client
-- PUT api/clients{clientId} to update a Client
+This sample demo makes use of various librairies:
+- Spring boot
+- Apache camel with swagger
+- Hibernate with H2 embedded database
 
-Testing the endpoints:
-- curl localhost:8080/api/health
-- curl -X POST -H "Content-Type: application/json" localhost:8080/api/clients -d '{"firstName": "Austin", "lastName": "Powars"}'
-- curl localhost:8080/api/clients/0 (Client ID)
-- curl -X PUT -H "Content-Type: application/json" localhost:8080/api/clients/0 -d '{"firstName": "Austin", "lastName": "Powarzz"}'
+by kevvlvl
+
+## Endpoints
+
+### Swagger API
+
+* curl http://localhost:8080/api/api-doc/swagger.json
+* curl http://localhost:8080/api/api-doc/swagger.yaml
+
+### API
+- GET api/health for custom API health check
+  - Ex: curl localhost:8080/api/health
+- POST api/clients to create a client
+  - Ex: curl -X POST -H "Content-Type: application/json" localhost:8080/api/clients -d '{"firstName": "Austin", "lastName": "Powars"}'
+- GET api/clients/{clientId} to get a client
+  - Ex: curl localhost:8080/api/clients/0 (Client ID)
+- PUT api/clients{clientId} to update a Client
+  - Ex: curl -X PUT -H "Content-Type: application/json" localhost:8080/api/clients/0 -d '{"firstName": "Austin", "lastName": "Powarzz"}'
--- src/main/java/com/kevvlvl/demo/camel/routes/CamelRoutes.java
+++ src/main/java/com/kevvlvl/demo/camel/routes/CamelRoutes.java
@@ -5,6 +5,8 @@ import com.kevvlvl.demo.camel.service.ClientServiceImpl;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,13 +15,30 @@ public class CamelRoutes extends RouteBuilder {
     @Override
     public void configure() {
         restConfiguration()
-                .component("servlet");
+                .component("servlet")
+                .bindingMode(RestBindingMode.json)
+                .dataFormatProperty("prettyPrint", "true")
+                .contextPath("/").port(8080)
+                .apiContextPath("/api-doc")
+                    .apiProperty("api.title", "Kevvlvl Demo API")
+                    .apiProperty("api.version", "1.0.0")
+                    .apiProperty("cors", "true");
 
         rest()
                 .get("/health").to("direct:getHealth")
-                .produces("application/json").get("/clients/{id}").to("direct:getClientById")
-                .consumes("application/json").produces("application/json").post("/clients").to("direct:postNewClient")
-                .consumes("application/json").produces("application/json").put("/clients/{id}").to("direct:putClient");
+                .produces("application/json").get("/clients/{id}")
+                    .description("Get Client by ID")
+                    .param().name("id").type(RestParamType.path).description("Client ID").dataType("int").endParam()
+                    .to("direct:getClientById")
+                .consumes("application/json").produces("application/json").post("/clients")
+                    .description("Create a client")
+                    .param().name("clientDetails").type(RestParamType.body).description("Client Info in JSON").dataType("String").endParam()
+                    .to("direct:postNewClient")
+                .consumes("application/json").produces("application/json").put("/clients/{id}")
+                    .description("Create a client")
+                    .param().name("id").type(RestParamType.path).description("ID of Client to update").dataType("int").endParam()
+                    .param().name("clientInfo").type(RestParamType.body).description("Client Info in JSON").dataType("String").endParam()
+                    .to("direct:putClient");
 
         from("direct:getHealth")
                 .log(LoggingLevel.INFO, "API getHealth")
@@ -31,7 +50,7 @@ public class CamelRoutes extends RouteBuilder {
                 .bean(ClientServiceImpl.class, "createClient(${body})")
                 .log(LoggingLevel.INFO, "API postNewClient - Created Client with ID=${body}");
 
-        from("direct:getClientById")
+        from("direct:getClienbtById")
                 .log(LoggingLevel.INFO, "API getClientById - Received query string=${header.id}")
                 .bean(ClientServiceImpl.class, "getByClientId(${header.id})")
                 .choice()
